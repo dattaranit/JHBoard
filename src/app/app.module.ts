8bbf05c0dbe36ac36567e85995af1c973acf0f86
Ranit <dattaranit@gmail.com>
2018-07-27 21:03:30 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestcomponentComponent
   ],
   imports: [
     BrowserModule
